764afc79e54fd528a7a013b007e2b9a5ab94779f
Minanik93 <129782955+Minanik93@users.noreply.github.com>
2023-04-16 22:05:52 +0430

    set axios and add id city & description

--- Project Plus/src/app.js
+++ Project Plus/src/app.js
@@ -1,1 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let city = "Behbahan";
+let apiKey = "866a4c8cf7e6c845713b2557bbaa0281";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
